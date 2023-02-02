008263ec2aa1997b909d73b607fb22b8b4c8a9c1
Swaleh Kavuma <swaleh.kavuma@disney.com>
2023-02-02 14:22:04 -0800

    Added Hello World

--- sample_flutter_app/lib/main.dart
+++ sample_flutter_app/lib/main.dart
@@ -1,28 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
+  runApp(
+    // Makes the Center widget the root of the widget tree. The widget tree consists of two widgets,
+    // the Center widget and its child, the Text widget. The framework forces the root widget to cover
+    // the screen, which means the text “Hello, world” ends up centered on screen. The text direction needs
+    // to be specified in this instance; when the MaterialApp widget is used, this is taken care of for you,
+    // as demonstrated later.
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    // When writing an app, you’ll commonly author new widgets that are subclasses of either StatelessWidget or StatefulWidget,
+    // depending on whether your widget manages any state. A widget’s main job is to implement a build() function, which describes
+    // the widget in terms of other, lower-level widgets. The framework builds those widgets in turn until the process bottoms out in
+    // widgets that represent the underlying RenderObject, which computes and describes the geometry of the widget.
+    const Center(
+      child: Text(
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const Text("Hello Flutter");
-  }
+    ),
+  );
 }
