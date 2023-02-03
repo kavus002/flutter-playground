49845a5a9af086639822111fd5379fc757db5c01
Swaleh Kavuma <swaleh.kavuma@disney.com>
2023-02-03 11:17:03 -0800

    Using Material Components

--- sample_flutter_app/lib/main.dart
+++ sample_flutter_app/lib/main.dart
@@ -1,89 +1,56 @@
 import 'package:flutter/material.dart';
 
+// Flutter provides a number of widgets that help you build apps that follow Material Design. A Material app starts with the MaterialApp widget,
+// which builds a number of useful widgets at the root of your app, including a Navigator, which manages a stack of widgets identified by strings,
+// also known as “routes”. The Navigator lets you transition smoothly between screens of your application.
+// Using the MaterialApp widget is entirely optional but a good practice.
 
-// Many Material Design widgets need to be inside of a MaterialApp to display properly, in order to inherit theme data.
-// Therefore, run the application with a MaterialApp.
-//
-// The MyAppBar widget creates a Container with a height of 56 device-independent pixels with an internal padding of 8 pixels, both on the left and the right.
-// Inside the container, MyAppBar uses a Row layout to organize its children. The middle child, the title widget, is marked as Expanded,
-// which means it expands to fill any remaining available space that hasn’t been consumed by the other children.
-// You can have multiple Expanded children and determine the ratio in which they consume the available space using the flex argument to Expanded.
-//
-// The MyScaffold widget organizes its children in a vertical column. At the top of the column it places an instance of MyAppBar, passing the app bar a Text widget to use as its title.
-// Passing widgets as arguments to other widgets is a powerful technique that lets you create generic widgets that can be reused in a wide variety of ways. Finally, MyScaffold uses an
-// Expanded to fill the remaining space with its body, which consists of a centered message.
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({required this.title, super.key});
+// Now that the code has switched from MyAppBar and MyScaffold to the AppBar and Scaffold widgets, and from material.dart, the app is starting to look a bit more Material.
+// For example, the app bar has a shadow and the title text inherits the correct styling automatically. A floating action button is also added.
+// Notice that widgets are passed as arguments to other widgets. The Scaffold widget takes a number of different widgets as named arguments, each of which are placed in the
+// Scaffold layout in the appropriate place. Similarly, the AppBar widget lets you pass in widgets for the leading widget, and the actions of the title widget. This pattern
+// recurs throughout the framework and is something you might consider when designing your own widgets.
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Flutter Tutorial',
+      home: TutorialHome(),
+    ),
+  );
+}
 
-  // Fields in a Widget subclass are always marked "final".
-  final Widget title;
+class TutorialHome extends StatelessWidget {
+  const TutorialHome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56.0, // in logical pixels
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      decoration: BoxDecoration(color: Colors.red[500]),
-      // Row is a horizontal, linear layout.
-      child: Row(
-        children: [
-          const IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null, // null disables the button
-          ),
-          // Expanded expands its child
-          // to fill the available space.
-          Expanded(
-            child: title,
-          ),
-          const IconButton(
+    // Scaffold is a layout for
+    // the major Material Components.
+    return Scaffold(
+      appBar: AppBar(
+        leading: const IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: const Text('Example title'),
+        actions: const [
+          IconButton(
             icon: Icon(Icons.search),
             tooltip: 'Search',
             onPressed: null,
           ),
         ],
       ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // Material is a conceptual piece
-    // of paper on which the UI appears.
-    return Material(
-      // Column is a vertical, linear layout.
-      child: Column(
-        children: [
-          MyAppBar(
-            title: Text(
-              'Example title',
-              style: Theme.of(context).primaryTextTheme.titleLarge,
-            ),
-          ),
-          const Expanded(
-            child: Center(
-              child: Text('Hello, world!'),
-            ),
-          ),
-        ],
+      // body is the majority of the screen.
+      body: const Center(
+        child: Text('Hello, world!'),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        onPressed: null,
+        child: Icon(Icons.add),
       ),
     );
   }
-}
-
-
-void main() {
-  runApp(
-    const MaterialApp(
-      title: 'My app', // used by the OS task switcher
-      home: SafeArea(
-        child: MyScaffold(),
-      ),
-    ),
-  );
 }
