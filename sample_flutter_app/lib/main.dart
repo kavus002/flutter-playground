234a089f7af472f030522cc279604fa36f1919c4
Swaleh Kavuma <swaleh.kavuma@disney.com>
2023-02-03 11:51:15 -0800

    Handling Gestures

--- sample_flutter_app/lib/main.dart
+++ sample_flutter_app/lib/main.dart
@@ -1,55 +1,45 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
-// Flutter provides a number of widgets that help you build apps that follow Material Design. A Material app starts with the MaterialApp widget,
-// which builds a number of useful widgets at the root of your app, including a Navigator, which manages a stack of widgets identified by strings,
-// also known as “routes”. The Navigator lets you transition smoothly between screens of your application.
-// Using the MaterialApp widget is entirely optional but a good practice.
-
-// Now that the code has switched from MyAppBar and MyScaffold to the AppBar and Scaffold widgets, and from material.dart, the app is starting to look a bit more Material.
-// For example, the app bar has a shadow and the title text inherits the correct styling automatically. A floating action button is also added.
-// Notice that widgets are passed as arguments to other widgets. The Scaffold widget takes a number of different widgets as named arguments, each of which are placed in the
-// Scaffold layout in the appropriate place. Similarly, the AppBar widget lets you pass in widgets for the leading widget, and the actions of the title widget. This pattern
-// recurs throughout the framework and is something you might consider when designing your own widgets.
 void main() {
   runApp(
     const MaterialApp(
-      title: 'Flutter Tutorial',
-      home: TutorialHome(),
+      home: Scaffold(
+        body: Center(
+          child: MyButton(),
+        ),
+      ),
     ),
   );
 }
 
-class TutorialHome extends StatelessWidget {
-  const TutorialHome({super.key});
+// The GestureDetector widget doesn’t have a visual representation but instead detects gestures made by the user. When the user taps the Container,
+// the GestureDetector calls its onTap() callback, in this case printing a message to the console. You can use GestureDetector to detect a variety
+// of input gestures, including taps, drags, and scales.
+//
+// Many widgets use a GestureDetector to provide optional callbacks for other widgets. For example, the IconButton, ElevatedButton, and FloatingActionButton
+// widgets have onPressed() callbacks that are triggered when the user taps the widget.
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Scaffold is a layout for
-    // the major Material Components.
-    return Scaffold(
-      appBar: AppBar(
-        leading: const IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
+    return GestureDetector(
+      onTap: () {
+        log('MyButton was tapped!');
+      },
+      child: Container(
+        height: 50.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 18.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
         ),
-        title: const Text('Example title'),
-        actions: const [
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
-      ),
-      // body is the majority of the screen.
-      body: const Center(
-        child: Text('Hello, world!'),
-      ),
-      floatingActionButton: const FloatingActionButton(
-        tooltip: 'Add', // used by assistive technologies
-        onPressed: null,
-        child: Icon(Icons.add),
+        child: const Center(
+          child: Text('Clickety Click on me to Engage'),
+        ),
       ),
     );
   }
