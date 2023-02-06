fee5d5b69bdfbdc1b8ac0d07ab5a760134f6e581
Swaleh Kavuma <swaleh.kavuma@disney.com>
2023-02-06 13:19:43 -0800

    Navigate with named routes

--- sample_flutter_app/lib/main.dart
+++ sample_flutter_app/lib/main.dart
@@ -1,39 +1,45 @@
 import 'package:flutter/material.dart';
 
-class FirstRoute extends StatelessWidget {
-  const FirstRoute({super.key});
+// In the Navigate to a new screen and back recipe, you learned how to navigate to a new screen by creating a new route and pushing it to the Navigator.
+//
+// However, if you need to navigate to the same screen in many parts of your app, this approach can result in code duplication.
+// The solution is to define a named route, and use the named route for navigation.
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('First Route'),
+        title: const Text('First Screen'),
       ),
       body: Center(
         child: ElevatedButton(
-          child: const Text('Open route'),
           onPressed: () {
-            // Navigate to second route when tapped.
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const SecondRoute()));
+            // Navigate to the second screen using a named route.
+            Navigator.pushNamed(context, '/second');
           },
+          child: const Text('Launch screen'),
         ),
       ),
     );
   }
 }
 
-class SecondRoute extends StatelessWidget {
-  const SecondRoute({super.key});
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Second Route'),
+        title: const Text('Second Screen'),
       ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
+            // Navigate back to the first screen by popping the current route
+            // off the stack.
             Navigator.pop(context);
           },
           child: const Text('Go back!'),
@@ -44,8 +50,16 @@ class SecondRoute extends StatelessWidget {
 }
 
 void main() {
-  runApp(const MaterialApp(
-    title: 'Navigation Basics',
-    home: FirstRoute(),
+  runApp(MaterialApp(
+    title: 'Named Routes Demo',
+    // Start the app with the "/" named route. In this case, the app starts
+    // on the FirstScreen widget.
+    initialRoute: '/',
+    routes: {
+      // When navigating to the "/" route, build the FirstScreen widget.
+      '/': (context) => const FirstScreen(),
+      // When navigating to the "/second" route, build the SecondScreen widget.
+      '/second': (context) => const SecondScreen(),
+    }
   ));
 }
